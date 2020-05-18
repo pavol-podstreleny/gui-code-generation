588a6058c530a7b3358518651d2102de515cedc9
Pavol Podstreleny <palo.podstreleny@gmail.com>
2020-05-18 14:33:30 +0800

    feat: show progress bar while running neural network
    Updated MainActivity.kt: created helper function for displaying/hiding views based on different states
    
    Updated ViewExtensions.kt: created extension for View, add show(), hide() method for showing, hiding Views

--- app/src/main/java/com/example/guicodegeneration/ui/MainActivity.kt
+++ app/src/main/java/com/example/guicodegeneration/ui/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.guicodegeneration.R
 import com.example.guicodegeneration.adapters.OnTestImageClicked
 import com.example.guicodegeneration.adapters.TestImageAdapter
+import com.example.guicodegeneration.utils.hide
+import com.example.guicodegeneration.utils.show
 import com.example.guicodegeneration.viewmodel.NeuralNetworkViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
@@ -41,6 +43,7 @@ class MainActivity : AppCompatActivity(), OnTestImageClicked {
         })
 
         viewModel.predictedTokens.observe(this, Observer {
+            contentDisplay(display = true)
             for (value in it) {
                 Toast.makeText(this.application, value.toString(), Toast.LENGTH_LONG).show()
             }
@@ -49,6 +52,33 @@ class MainActivity : AppCompatActivity(), OnTestImageClicked {
     }
 
     override fun onClick(bitmap: Bitmap, fileName: String) {
+        contentDisplay(display = false)
         viewModel.predictTokens(bitmap, Uri.parse(fileName))
     }
+
+    private fun contentDisplay(display: Boolean = true) {
+        when (display) {
+            true -> {
+                test_images_rv.show()
+                test_image_tv.show()
+                load_custom_bt.show()
+                custom_image_tv.show()
+
+                // hide loading
+                loading_pb.hide()
+                generating_layout_tv.hide()
+            }
+            false -> {
+                test_images_rv.hide()
+                test_image_tv.hide()
+                load_custom_bt.hide()
+                custom_image_tv.hide()
+
+                // show loading
+                loading_pb.show()
+                generating_layout_tv.show()
+            }
+        }
+    }
+
 }
