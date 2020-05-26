8b02c6c9c2de156a256869e979bf4e30a3af9a97
Pavol Podstreleny <palo.podstreleny@gmail.com>
2020-05-26 20:25:47 +0800

    feat: Added option for loading custom layout
    Updated files:
    MainActivity.kt: add button handler that starts activity that handles loading the custom layouts. When custom layout is selected onActivityResult starts token generation process
    activity_main.xml: add id for constraintLayout
    fragment_empty_layout.xml: center text when displaying error
    AndroidManifest.xml: add activity from cropping library
    build.gradle: add cropping library

--- app/src/main/java/com/example/guicodegeneration/ui/MainActivity.kt
+++ app/src/main/java/com/example/guicodegeneration/ui/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.guicodegeneration.ui
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Rect
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.guicodegeneration.R
@@ -14,6 +18,9 @@ import com.example.guicodegeneration.ui.code_generation.CodeGenerationActivity
 import com.example.guicodegeneration.utils.hide
 import com.example.guicodegeneration.utils.show
 import com.example.guicodegeneration.viewmodel.NeuralNetworkViewModel
+import com.google.android.material.snackbar.Snackbar
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 
@@ -55,6 +62,36 @@ class MainActivity : AppCompatActivity(), OnTestImageClicked {
             startActivity(intent)
         })
 
+        load_custom_bt.setOnClickListener {
+            CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setInitialCropWindowRectangle(Rect(0, 0, 800, 1200))
+                .start(this)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            val result = CropImage.getActivityResult(data)
+            if (resultCode == Activity.RESULT_OK) {
+                val resultUri: Uri = result.uri
+                try {
+                    val bitmap = MediaStore.Images.Media.getBitmap(
+                        contentResolver,
+                        resultUri
+                    )
+                    contentDisplay(false)
+                    viewModel.predictTokens(bitmap, uri = resultUri)
+                } catch (e: Exception) {
+                    finish()
+                }
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                val error = result.error
+                Snackbar.make(main, error.toString(), Snackbar.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onClick(bitmap: Bitmap, fileName: String) {
